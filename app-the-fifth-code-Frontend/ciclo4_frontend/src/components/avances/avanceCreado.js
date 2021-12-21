0ffce287d5e1ed16bcff9e39e77929127ae441fa
Lacatanino <catalinaninom@gmail.com>
2021-12-20 20:31:47 -0500

    Backend y Frontend

---
+++ app-the-fifth-code-Frontend/ciclo4_frontend/src/components/avances/avanceCreado.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+
+
+const CreateAvance =()=>{
+ 
+        return(
+            
+            <h1>Se creó su avance con éxito</h1>
+        )
+}
+export default CreateAvance;
---
+++ index.js
@@ -0,0 +1,92 @@
+require('./infraestructura/conectionDB')
+const typeDefs = require('./typeDef')
+const resolvers = require('./resolver')
+//mm
+const express = require('express')
+const  authRoute = require('./routes/auth.routes')
+const {validarToken, admin} = require('./middleware/authjwt')
+const jwt = require('jsonwebtoken');
+const key = 'CLAVEDIFICIL';
+
+const cool = require('cool-ascii-faces');
+const path = require('path');
+const PORT = process.env.PORT || 9091; //Para deploy en heroku
+// const PORT = 3000; 
+
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+// //   .get('/', (req, res) => res.render('./app-the-fifth-code-Frontend/ciclo4_frontend/src/index.js'))
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+/*
+const proyectoAguas =new ProyectoModel({
+    name:'Proyecto de aguas residuales'
+})
+proyectoAguas.save((err,document)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+});
+
+const consultaProyectos = async ()=>{
+    return await ProyectoModel.find({});
+}
+
+
+api.get('/proyectos',(request,response)=>{
+    consultaProyectos().then(function(resultado){
+        response.json({ proyectos1 :resultado});
+    })
+});*/
+
+const {ApolloServer} = require('apollo-server-express')
+
+const iniciarServidor =async()=>{
+    const api = express();
+    //Se sirve react build como archivo estatico
+    api.use(express.static(path.join(__dirname, "/app-the-fifth-code-Frontend/ciclo4_frontend/", "build")));
+    api.use(express.static("public"));
+    api.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'app-the-fifth-code-Frontend/ciclo4_frontend/build', 'index.html'));
+    });
+    //
+    const apollo= new ApolloServer(
+        {
+            typeDefs,
+            resolvers
+            // context: ({req}) => {
+            //     const token = req.headers.authorization;
+            //     try{
+            //         const perfil = jwt.verify(token, key)
+            //         if(perfil){
+            //             rol = perfil.role
+            //             return {rol}
+            //         }
+            //     }catch(error){
+            //         console.log(error)
+            //     }
+            //     return{}
+            
+        });
+    await apollo.start()
+    apollo.applyMiddleware({app:api,path:'/consulta'})
+    /*api.use((request, response)=>{
+        response.send('Hola')
+    })*/
+    api.use(express.json())
+    
+    api.use('/api', authRoute)
+    api.get('/api/dashboard', [validarToken, admin], (request, response)=>{
+        response.json("Soy el dashboard");
+    })
+    api.listen(PORT,()=>console.log("Inicio server en puerto",PORT))
+}
+iniciarServidor()
+
+
+
+
